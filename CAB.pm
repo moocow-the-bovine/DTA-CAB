277e191c60b2aee393aade2f319eae7964bcc475
Bryan Jurish <jurish@bbaw.de>
2010-02-08 14:52:51 +0000

    + updated to v1.02
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@3571 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB.pm
+++ CAB.pm
@@ -60,7 +60,7 @@ use strict;
 ## Constants
 ##==============================================================================
 
-our $VERSION = 1.01;
+our $VERSION = 1.02;
 our @ISA = qw(DTA::CAB::Chain::DTA); ##-- inherit from default analyzer (v0.x-compatibility hack)
 
 
