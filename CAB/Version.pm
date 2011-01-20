009170bfea5b5e65ef3ce366feb1ee5159619845
Bryan Jurish <jurish@bbaw.de>
2011-01-20 11:06:25 +0000

    #
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@4760 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -1,5 +1,5 @@
 ##-- automatically generated file: DO NOT EDIT!
 package DTA::CAB::Version;
-$DTA::CAB::VERSION = "1.12";
-$DTA::CAB::SVNVERSION = "$HeadURL: svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk/Makefile.PL $ $Id: Makefile.PL 4476 2010-11-30 14:19:08Z moocow $";
+$DTA::CAB::VERSION = '1.12';
+$DTA::CAB::SVNVERSION = '$HeadURL: svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk/Makefile.PL$ $Id: Makefile.PL 4759 2011-01-20 11:05:11Z moocow$';
 1;
