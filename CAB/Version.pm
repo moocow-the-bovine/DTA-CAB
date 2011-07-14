659337287a76092b5027918822263231aa4db24b
Bryan Jurish <jurish@bbaw.de>
2011-07-14 13:05:41 +0000

    + updated to v1.17: forking dta-cab-analyze.perl
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@5427 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -1,5 +1,5 @@
 ##-- automatically generated file: DO NOT EDIT!
 package DTA::CAB::Version;
-$DTA::CAB::VERSION = '1.16';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 5423:5425';
+$DTA::CAB::VERSION = '1.17';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 5423:5426M';
 1;
