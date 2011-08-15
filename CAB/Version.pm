27d5f0f90a19a01fa466b71fcf323422f50fba72
Bryan Jurish <jurish@bbaw.de>
2011-08-15 13:02:39 +0000

    + version, analyze verbosity for spawn
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@5739 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -1,5 +1,5 @@
 ##-- automatically generated file: DO NOT EDIT!
 package DTA::CAB::Version;
 $DTA::CAB::VERSION = '1.18';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 5724:5733M';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 5724:5734';
 1;
