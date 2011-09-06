7f2bf52980ed6bde3f2a90dd7496f0aeb527dab8
Bryan Jurish <jurish@bbaw.de>
2011-09-06 10:52:11 +0000

    + added CDB_File dep to Makefile.PL
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@5880 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -1,5 +1,5 @@
 ##-- automatically generated file: DO NOT EDIT!
 package DTA::CAB::Version;
 $DTA::CAB::VERSION = '1.20';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 5872:5873';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 5879M';
 1;
