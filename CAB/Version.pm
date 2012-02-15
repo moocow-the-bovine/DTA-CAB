609fb97554e0e11b3c6e13d96a8ad5e8ce40deec
Bryan Jurish <jurish@bbaw.de>
2012-02-15 13:19:14 +0000

    + updated MANIFEST: remove CAB/Format/JSON/*.pm, CAB/Format/YAML/*.pm
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@6639 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -1,5 +1,5 @@
 ##-- automatically generated file: DO NOT EDIT!
 package DTA::CAB::Version;
 $DTA::CAB::VERSION = '1.24';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 6637';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 6638M';
 1;
