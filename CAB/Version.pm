f94dba03f211b578453dfcf8dc8833abc9ce013d
Bryan Jurish <jurish@bbaw.de>
2012-06-25 11:25:57 +0000

    + tei re-fix (revision 7415:7416 broke DTAQ)
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@7545 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -1,5 +1,5 @@
 ##-- automatically generated file: DO NOT EDIT!
 package DTA::CAB::Version;
 $DTA::CAB::VERSION = '1.25';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 6893:7155';
+$DTA::CAB::SVNVERSION = 'svn+ssh://moocow@odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 7416M';
 1;
