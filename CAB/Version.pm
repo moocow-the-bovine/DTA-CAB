106759bcd55ce4e7733dff9aba5c6d71a8f4058d
Bryan Jurish <jurish@bbaw.de>
2012-07-24 14:06:47 +0000

    + added dta-cab-eval.perl
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@7715 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -1,5 +1,5 @@
 ##-- automatically generated file: DO NOT EDIT!
 package DTA::CAB::Version;
-$DTA::CAB::VERSION = '1.28';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 7668:7679M';
+$DTA::CAB::VERSION = '1.29';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 7687:7702M';
 1;
