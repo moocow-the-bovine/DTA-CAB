c4e423713d56aa3fb6cea90c017ea44ba46e3fff
Bryan Jurish <jurish@bbaw.de>
2012-10-05 07:32:38 +0000

    + v1.33: updates for dta-tokwrap v0.37
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@8223 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -1,5 +1,5 @@
 ##-- automatically generated file: DO NOT EDIT!
 package DTA::CAB::Version;
-$DTA::CAB::VERSION = '1.32';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 8157';
+$DTA::CAB::VERSION = '1.33';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 8222M';
 1;
