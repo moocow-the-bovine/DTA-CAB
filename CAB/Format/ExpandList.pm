6cae1dd8fcef6a7ebdda41dba278abf4521bbf56
Bryan Jurish <jurish@bbaw.de>
2013-03-11 13:13:31 +0000

    + ExpandList fix
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@9224 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Format/ExpandList.pm
+++ CAB/Format/ExpandList.pm
@@ -48,7 +48,7 @@ BEGIN {
 ## + inherited from DTA::CAB::Format::TT
 sub new {
   my $that = shift;
-  return $that->SUPER::new(keys=>[qw(text xlit eqpho eqrw eqlemma eqtagh)]);
+  return $that->SUPER::new(keys=>[qw(text xlit eqpho eqrw eqlemma eqtagh)],@_);
 }
 
 ##==============================================================================
