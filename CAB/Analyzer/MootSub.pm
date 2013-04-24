3726c9bc833edda75280fd3799c70c25f2d9bc8c
Bryan Jurish <jurish@bbaw.de>
2013-04-24 12:12:55 +0000

    + removed xlemma stuff again
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@9506 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Analyzer/MootSub.pm
+++ CAB/Analyzer/MootSub.pm
@@ -72,14 +72,14 @@ sub analyzeSentences {
 
   ##-- Step 3: lemma-extraction & tag-sensitive lemmatization hacks
   my %cache = qw(); ##-- $cache{"$word\t$tag"} = $lemma
-  my ($w,$t,$l,$key,$ma,$maa,%l2d, $ld,$ld0, $l0,$a0,$xl);
+  my ($w,$t,$l,$key,$ma,$maa,%l2d, $ld, $l0,$ld0);
   foreach $tok (@$toks) {
     $m      = $tok->{$mlabel};
     ($w,$t) = @$m{qw(word tag)};
     $key    = "$w/$t";
     if (defined($l=$cache{$key})) {
       ##-- cached value
-      @$m{qw(lemma xlemma)}=@$l;
+      $m->{lemma} = $l;
       next;
     }
 
@@ -100,14 +100,14 @@ sub analyzeSentences {
 	$l =~ s/[\x{ac}]//g;
 	$l = lc($l);
 	$l =~ s/(?:^|(?<=[\-\_]))(.)/\U$1\E/g if ($t =~ /^N/); ##-- implicitly upper-case NN, NE (in case e.g. 'NE' \in $xytags)
-	@$m{qw(lemma xlemma)} = @{$cache{$key}} = ($l,$l);
+	$m->{lemma} = $cache{$key} = $l;
       }
     else
       {
 	##-- extract lemma from "best" analysis
 	%l2d = qw();
 
-	$l0 = $ld0 = $a0 = undef;
+	$l0 = $ld0 = undef;
 	foreach (@$ma) {
 	  ##-- get lemma distance
 	  $l   = $_->{lemma};
@@ -117,10 +117,8 @@ sub analyzeSentences {
 	  next if (defined($ld0) && $ld0 <= $ld);
 	  $ld0 = $ld;
 	  $l0  = $l;
-	  $a0  = $_;
 	}
-	($xl = $a0->{details}) =~ s/\[.*$//; ##-- trim everything after first non-character symbol
-	@$m{qw(lemma xlemma)} = @{$cache{$key}} = ($l0,$xl);
+	$m->{lemma} = $cache{$key} = $l0;
       }
   }
 
