27daace03e53e1c798ac0cfa0eb052921528fd88
Bryan Jurish <jurish@bbaw.de>
2013-05-03 11:35:13 +0000

    + v1.41: added LangId::Simple analyzer, built into DTA chain as 'langid'
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@9621 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -1,5 +1,5 @@
 ##-- automatically generated file: DO NOT EDIT!
 package DTA::CAB::Version;
 $DTA::CAB::VERSION = '1.41';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 9551:9564M';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 9618:9620M';
 1;
