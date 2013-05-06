23af3fbc62b584522110b908e5e3504e2ad98bf1
Bryan Jurish <jurish@bbaw.de>
2013-05-06 11:03:45 +0000

    + lang-guesser updates: unicode hacks
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@9644 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Analyzer/LangId/Simple.pm
+++ CAB/Analyzer/LangId/Simple.pm
@@ -70,7 +70,7 @@ sub analyzeTypes {
       if    ($_->{text} =~ /^\p{Greek}+$/)  { push(@l, 'el'); }
       elsif ($_->{text} =~ /^\p{Hebrew}+$/) { push(@l, 'he'); }
       elsif ($_->{text} =~ /^\p{Arabic}+$/) { push(@l, 'ar'); }
-      elsif ($_->{text} =~ /[[:alpha:]]/ && !~ $_->{text} !~ /\p{Latin}/) { push(@l,'xy'); }
+      elsif ($_->{text} =~ /[[:alpha:]]/ && $_->{text} !~ /\p{Latin}/) { push(@l,'xy'); }
     }
     push(@l, 'la') if ($_->{mlatin});
     push(@l, $l0) if ($l0 && $_->{morph} && $_->{msafe} && grep {$_->{hi} !~ /\[_(?:FM|NE)\]/} @{$_->{morph}});
