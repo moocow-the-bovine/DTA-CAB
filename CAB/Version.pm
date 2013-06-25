6c938e231d654653377e2ce6aabab69a6e717f63
Bryan Jurish <jurish@bbaw.de>
2013-06-25 13:49:48 +0000

    + v1.43: first version with TCF support
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@9996 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -1,5 +1,5 @@
 ##-- automatically generated file: DO NOT EDIT!
 package DTA::CAB::Version;
-$DTA::CAB::VERSION = '1.42';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 9989';
+$DTA::CAB::VERSION = '1.43';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 9989:9995M';
 1;
