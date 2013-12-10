40b192edaef16d5ad055d9a41abb87bdd8b2b00a
Bryan Jurish <jurish@bbaw.de>
2013-12-10 13:41:57 +0000

    * v1.50: new raw input modes
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@11369 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -1,5 +1,5 @@
 ##-- automatically generated file: DO NOT EDIT!
 package DTA::CAB::Version;
-$DTA::CAB::VERSION = '1.49';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 11362';
+$DTA::CAB::VERSION = '1.50';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 11368';
 1;
