879cb13f01e7810553dc9ae417e020c2b13be107
Bryan Jurish <jurish@bbaw.de>
2014-11-28 08:40:59 +0000

    * v1.65 : tcf tweaks: encode tei in textCorpus/textSource as schema trunk describes
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@13409 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -1,5 +1,5 @@
 ##-- automatically generated file: DO NOT EDIT!
 package DTA::CAB::Version;
-$DTA::CAB::VERSION = '1.64';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 13369M';
+$DTA::CAB::VERSION = '1.65';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 13408';
 1;
