moocow-the-bovine/DTA-CAB

commit 32932f62048fe8474a0bebefdc13a093897e6a26

+ added CAB/Thread/Pool.pm : generic thread pool + added CAB/Thread/Semaphores.pm : generic semaphore pool (best to remove it again in favor of analyzer-local semaphores) + added semaphore wrapper downup() in CAB/Utils.pm under tag ':threads' + thread tests in tests/thread/test-gfsm-pool.perl : looks pretty good + started adding thread-savvy code to CAB/Analyzer.pm and CAB/Analyzer/Automaton sub-hierarchy   : problem here is where to insert the semaphore pseudo-locking wrappers
git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@5410 4a515de8-beb3-11dc-84d6-2b654028afac

CAB/Logger.pm
@@ -85,6 +85,7 @@ log4perl.oneMessagePerAppender = 1     ##-- suppress duplicate messages to the s
   $cfg .= "
 ##-- Appenders: Utilities
 log4perl.PatternLayout.cspec.G = sub { return File::Basename::basename(\"$::0\"); }
+log4perl.PatternLayout.cspec.i = sub { return threads->tid(); }
 ";
 
   ##-- appender: stderr
@@ -94,7 +95,7 @@ log4perl.appender.AppStderr = Log::Log4perl::Appender::Screen
 log4perl.appender.AppStderr.stderr = 1
 log4perl.appender.AppStderr.binmode = :utf8
 log4perl.appender.AppStderr.layout = Log::Log4perl::Layout::PatternLayout
-log4perl.appender.AppStderr.layout.ConversionPattern = %G[%P] %p: %c: %m%n
+log4perl.appender.AppStderr.layout.ConversionPattern = %G[%P/%i] %p: %c: %m%n
 ";
 
   ##-- appender: syslog
@@ -127,7 +128,7 @@ log4perl.appender.AppFile.mode = append
 log4perl.appender.AppFile.size = 10485760
 log4perl.appender.AppFile.max  = 10
 log4perl.appender.AppFile.layout = Log::Log4perl::Layout::PatternLayout
-log4perl.appender.AppFile.layout.ConversionPattern = %d{yyyy-MM-dd HH:mm:ss} [%P] (%p) %c: %m%n
+log4perl.appender.AppFile.layout.ConversionPattern = %d{yyyy-MM-dd HH:mm:ss} [%P/%i] (%p) %c: %m%n
 ";
   }
   elsif ($opts{file}) {
@@ -138,8 +139,8 @@ log4perl.appender.AppFile = Log::Log4perl::Appender::File
 log4perl.appender.AppFile.filename = $opts{file}
 log4perl.appender.AppFile.mode = append
 log4perl.appender.AppFile.utf8 = 1
-log4perl.appender.AppFile.layout = Log::Log4perl::Layout::PatternLayout
-log4perl.appender.AppFile.layout.ConversionPattern = %d{yyyy-MM-dd HH:mm:ss} [%P] (%p) %c: %m%n
+log4perl.appender.AppFile.layout = Log::Log4perl::Layout::PatternLayoutl
+log4perl.appender.AppFile.layout.ConversionPattern = %d{yyyy-MM-dd HH:mm:ss} [%P/%i] (%p) %c: %m%n
 ";
   }
 

CAB/Analyzer.pm
@@ -98,6 +98,49 @@ sub typeKeys {
   return $_[0]{typeKeys} ? @{$_[0]{typeKeys}} : (defined($_[0]{label}) ? ($_[0]{label}) : qw());
 }
 
+##==============================================================================
+## Methods: Threaded Operation
+##==============================================================================
+
+## $anl = $anl->threadPrepare()
+## $anl = $anl->threadPrepare(\%opts)
+##  + prepares $anl to run in threaded mode
+##  + should be called from main thread
+##  + default implementation does:
+##    - creates a semaphore $anl->{$anl->semaphoreKey()} if not already present
+##    - propagates call to subAnalyzers(\%opts)
+sub threadPrepare {
+  use Thread::Semaphore;
+  my $anl = shift;
+  my $skey = $anl->semaphoreKey;
+  $anl->{$skey} = Thread::Semaphore->new() if (defined($skey));
+  $_->threadPrepare(@_) foreach (@{$anl->subAnalyzers(@_)});
+  return $anl;
+}
+
+## $key_or_undef = $anl->semaphoreKey()
+##  + returns semaphore key for default threadPrepare(), or undef (default)
+sub semaphoreKey {
+  return undef;
+}
+
+## undef = $anl->threadInit()
+## undef = $anl->threadInit(\%opts)
+##  + initializes analyzer to run in a new thread; called from sub-thread
+##  + default implementation just propagates call to subAnalyzers(\%opts)
+sub threadInit {
+  my $anl = shift;
+  $_->threadInit(@_) foreach (@{$anl->subAnalyzers(@_)});
+}
+
+## undef = $anl->threadFree()
+## undef = $anl->threadFree(\%opts)
+##  + shuts down analyzer running in a sub-thread
+##  + default implementation just propagates call to subAnalyzers(\%opts)
+sub threadFree {
+  my $anl = shift;
+  $_->threadFree(@_) foreach (@{$anl->subAnalyzers(@_)});
+}
 
 ##==============================================================================
 ## Methods: I/O

CAB/Analyzer/Automaton.pm
@@ -7,6 +7,12 @@
 package DTA::CAB::Analyzer::Automaton;
 use DTA::CAB::Analyzer ':child';
 use DTA::CAB::Unify ':all';
+use DTA::CAB::Utils ':threads';
+
+use threads;
+use Thread::Semaphore;
+use Data::Structure::Utils 'unbless';
+
 use Gfsm;
 use Encode qw(encode decode);
 use IO::File;
@@ -78,7 +84,8 @@ our $DEFAULT_ANALYZE_SET = '$_->{$lab} = ($wa && @$wa ? [@$wa] : undef);';
 ##     labh => \%sym2lab,  ##-- (?) label hash:  $sym2lab{$labSym} = $labId;
 ##     laba => \@lab2sym,  ##-- (?) label array:  $lab2sym[$labId]  = $labSym;
 ##     labc => \@chr2lab,  ##-- (?)chr-label array: $chr2lab[ord($chr)] = $labId;, by unicode char number (e.g. unpack('U0U*'))
-##     result=>$resultfst, ##-- (child classes only) e.g. result fst
+##     #result=>$resultfst, ##-- (child classes only) e.g. result fst
+##     tsem => $tsem,      ##-- Thread::Semaphore object to use for pseudo-locking
 ##
 ##     ##-- INHERITED from DTA::CAB::Analyzer
 ##     label => $label,    ##-- analyzer label (default: from analyzer class name)
@@ -94,7 +101,7 @@ sub new {
 			      ##-- analysis objects
 			      fst=>undef,
 			      lab=>undef,
-			      result=>undef,
+			      #result=>undef,
 			      labh=>{},
 			      laba=>[],
 			      labc=>[],
@@ -133,12 +140,41 @@ sub clear {
   ##-- analysis objects
   delete($aut->{fst});
   delete($aut->{lab});
-  delete($aut->{result});
+  delete($aut->{result}); ##-- should be undef anyways
   %{$aut->{labh}} = qw();
   @{$aut->{laba}} = qw();
   @{$aut->{labc}} = qw();
 
   return $aut;
+}
+
+##==============================================================================
+## Methods: Threads
+##==============================================================================
+
+## $key_or_undef = $anl->semaphoreKey()
+##  + returns semaphore key for default threadPrepare(), or undef (default)
+##  + default returns 'tsem'
+sub semaphoreKey {
+  return 'tsem';
+}
+
+## undef = $anl->threadInit()
+## undef = $anl->threadInit(\%opts)
+##  + initializes analyzer to run in a new thread; called from sub-thread
+##  + default implementation just propagates call to subAnalyzers(\%opts)
+##INHERITED
+
+## undef = $anl->threadFree()
+## undef = $anl->threadFree(\%opts)
+##  + shuts down analyzer running in a sub-thread
+##  + default implementation just propagates call to subAnalyzers(\%opts)
+##  + override un-blesses opaque C pointers to avoid double-free
+sub threadFree {
+  my $anl = shift;
+  unbless($anl->{fst}) if (defined($anl->{fst}));
+  unbless($anl->{lab}) if (defined($anl->{lab}));
+  unbless($anl->{result}) if (defined($anl->{result}));
 }
 
 
@@ -205,9 +241,15 @@ sub loadFst {
   $aut->{fst} = $aut->fstClass->new() if (!defined($aut->{fst}));
   $aut->{fst}->load($fstfile)
     or $aut->logconfess("loadFst(): load failed for '$fstfile': $!");
-  $aut->{result} = $aut->{fst}->shadow; #if (defined($aut->{result}) && $aut->{fst}->can('shadow'));
+  #$aut->{result} = $aut->{fst}->shadow; #if (defined($aut->{result}) && $aut->{fst}->can('shadow'));
   delete($aut->{_analyze});
   return $aut;
+}
+
+## $result = $aut->resultFst()
+##  + returns empty result FST
+sub resultFst {
+  return $_[0]{fst}->shadow;
 }
 
 ##--------------------------------------------------------------
@@ -300,7 +342,7 @@ sub analyzeTypes {
   ##-- setup common variables
   my $fst    = $aut->{fst};
   my $fst_ok = $aut->fstOk();
-  my $result = $aut->{result};
+  my $result = $aut->resultFst();
   my $lab    = $aut->{lab};
   my $labc   = $aut->{labc};
   my $laba   = $aut->{laba};
@@ -582,7 +624,6 @@ Constuctor.
  labh => \%sym2lab,  ##-- (?) label hash:  $sym2lab{$labSym} = $labId;
  laba => \@lab2sym,  ##-- (?) label array:  $lab2sym[$labId]  = $labSym;
  labc => \@chr2lab,  ##-- (?)chr-label array: $chr2lab[ord($chr)] = $labId;, by unicode char number (e.g. unpack('U0U*'))
- result=>$resultfst, ##-- (child classes only) e.g. result fst
  ##
  ##-- INHERITED from DTA::CAB::Analyzer
  label => $label,    ##-- analyzer label (default: from analyzer class name)

CAB/Analyzer/Automaton/Gfsm.pm
@@ -31,7 +31,7 @@ sub new {
 			      ##-- analysis objects
 			      fst=>undef, #Gfsm::Automaton->new,
 			      lab=>undef, #Gfsm::Alphabet->new,
-			      result=>undef, #Gfsm::Automaton->new,
+			      #result=>undef, #Gfsm::Automaton->new,
 
 			      ##-- user args
 			      @_

CAB/Analyzer/Automaton/Gfsm/XL.pm
@@ -162,10 +162,17 @@ sub loadCascade {
   }
   $aut->{fst} = Gfsm::XL::Cascade::Lookup->new($csc);
   $aut->setLookupOptions($aut);
-  $aut->{result} = Gfsm::Automaton->new($csc->semiring_type);  ##-- reset result automaton
+  #$aut->{result} = Gfsm::Automaton->new($csc->semiring_type);  ##-- reset result automaton
   delete($aut->{_analyze});
   return $aut;
 }
+
+## $result = $aut->resultFst()
+##  + returns empty result FST
+sub resultFst {
+  return Gfsm::Automaton->new($_[0]{fst}->cascade->semiring_type);
+}
+
 
 ##--------------------------------------------------------------
 ## Methods: I/O: Input: Labels

CAB/HttpProtocol.pod
@@ -216,7 +216,7 @@ as output by $fmt-E<gt>toString().
 ##========================================================================
 =pod
 
-=head2 Caching
+=head2 Response Caching
 
 As of DTA::CAB version 1.16, DTA::CAB::Server::HTTP supports basic in-memory
 caching of responses for GET requests.  Client requests may set the
@@ -227,6 +227,8 @@ its generated response (although this option has no effect on a previously
 cached response for the same URI, if one exists).  If the response returned
 by the server was drawn from the server-internal cache, it will contain the
 header C<X-Cached: 1>.
+
+=cut
 
 ##======================================================================
 ## Footer

CAB/Thread/Pool.pm
@@ -1,0 +1,248 @@
+## -*- Mode: CPerl -*-
+##
+## File: DTA::CAB::Thread::Pool.pm
+## Author: Bryan Jurish <jurish@uni-potsdam.de>
+## Description: generic thread pool for DTA::CAB
+
+package DTA::CAB::Thread::Pool;
+use threads;
+use Thread::Queue;
+use DTA::CAB::Logger;
+use Carp;
+use strict;
+
+##==============================================================================
+## Globals
+##==============================================================================
+
+our @ISA = qw(DTA::CAB::Logger);
+
+##==============================================================================
+## Constructors etc.
+##==============================================================================
+
+## $tp = CLASS_OR_OBJ->new(%args)
+##  + %$tp, %args:
+##    {
+##     ##-- High-level options
+##     njobs  => $n_threads,    ##-- number of worker threads to allocate (default=0)
+##     init   => \&init,        ##-- called as init($tp) in each worker thread on creation (default: $tp->can('init'))
+##     work   => \&work,        ##-- called as work($tp,@$args) in a worker thread to process a queue item (default: $tp->can('work'))
+##     catch  => \&catch,       ##-- called as catch($tp,$@) on sub-thread die() (default: $tp->can('catch'))
+##     free   => \&free,        ##-- called as free($tp) in each worker thread before destruction (default: $tp->can('free'))
+##
+##     catchLogLevel     => $level,   ##-- log-level for messages caught with default catch() [default: 'fatal']
+##     catchKillsProcess => $bool,    ##-- if true (default), default 'catch' method will exit() the whole process
+##     catchKillsThread  => $bool,    ##-- if true (default), default 'catch' method will exit() the current thread
+##     ##
+##     ##-- Low-level data
+##     queue   => $queue,       ##-- underlying Thread::Queue object
+##     threads => \@threads,    ##-- worker threads
+##    }
+sub new {
+  my $that = shift;
+  my $tp = bless {
+		  njobs => 0,
+		  init  => $that->can('init'),
+		  work  => $that->can('work'),
+		  catch => $that->can('catch'),
+		  free  => $that->can('free'),
+		  catchLogLevel => 'fatal',
+		  catchKillsProcess => 1,
+		  catchKillsThread  => 1,
+		  queue => undef,
+		  threads => [],
+		  @_,
+		 }, ref($that)||$that;
+
+  ##-- initialize
+  $tp->populate();
+
+  return $tp;
+}
+
+##==============================================================================
+## Methods: Thread Maintainence
+
+## $tp = $tp->populate()
+##  + ensures that at least $tp->{njobs} worker threads are defined in $tp->{threads}
+sub populate {
+  my $tp = shift;
+  $tp->{queue} = Thread::Queue->new() if (!$tp->{queue});
+  while (@{$tp->{threads}} < $tp->{njobs}) {
+    push(@{$tp->{threads}}, threads->create($tp->can('threadMain'), $tp));
+  }
+  return $tp;
+}
+
+## $tp = $tp->abort()
+## $tp = $tp->abort($SIGNAL)
+##  + kills child threads
+sub abort {
+  my ($tp,$sig) = @_;
+  $sig = 'TERM' if (!defined($sig));
+  my ($t);
+  while (defined($t=shift(@{$tp->{threads}}))) {
+    $t->kill($sig);
+    $t->join() if ($t->is_joinable);
+  }
+  return $tp;
+}
+
+## $tp = $tp->reset()
+##  + aborts all running threads, empties the queue, and re-populates
+sub reset {
+  my $tp = shift;
+  $tp->abort();
+  $tp->{queue}->dequeue($tp->{queue}->pending);
+  $tp->populate();
+  return $tp;
+}
+
+## @threads = $tp->list
+##  + list of all sub-threads
+sub list {
+  return @{$_[0]{threads}};
+}
+
+## @threads = $tp->detached()
+##  + list of all detached sub-threads
+sub detached {
+  return grep {$_->is_detached} @{$_[0]{threads}};
+}
+
+## @threads = $tp->undetached()
+##  + list of all non-detached sub-threads
+sub undetached {
+  return grep {!$_->is_detached} @{$_[0]{threads}};
+}
+
+## @threads = $tp->running()
+##  + list of all running sub-threads
+sub running {
+  return grep {$_->is_running} @{$_[0]{threads}};
+}
+
+## @threads = $tp->joinable()
+##  + list of all joinable (done running, not detached and not yet joined) sub-threads
+sub joinable {
+  return grep {$_->is_joinable} @{$_[0]{threads}};
+}
+
+## $tp = $tp->join()
+##  + join()s on all undetached sub-threads and removes them from $tp->{threads}
+sub join {
+  my $tp = shift;
+  my @detached = $tp->detached;
+  $_->join() foreach ($tp->undetached);
+  @{$tp->{threads}} = @detached;
+  return $tp;
+}
+
+## $tp = $tp->detach()
+##  + detach()es any as-et undetached sub-threads
+##  + prohibits subsequent join(), abort() from joining any threads
+sub detach {
+  my $tp = shift;
+  $_->detach() foreach ($tp->undetached);
+  return $tp;
+}
+
+##==============================================================================
+## Methods: Queue Maintainence
+
+## $tp = $tp->enqueue(\@args, ...)
+##  + enqueues a new set of arguments to be processed by $tp->{work}->($tp,@args)
+##  + you MUST enqueue() undef (or call $tp->finish()) after enqueuing all 
+sub enqueue {
+  $_[0]{queue}->enqueue(@_[1..$#_]);
+  return $_[0];
+}
+
+## $args_or_undef = $tp->dequeue()
+## $args_or_undef = $tp->dequeue($n_items)
+##  + dequeue item(s) from the queue (blocks)
+sub dequeue {
+  return $_[0]{queue}->dequeue(@_[1..$#_]);
+}
+
+## $n = $tp->pending()
+##  + returns number of pending items in the queue
+sub pending {
+  return $_[0]{queue}->pending();
+}
+
+## $tp = $tp->finish()
+##  + appends EOQ marker (undef) to the queue and calls $tp->join()
+sub finish {
+  my $tp = shift;
+  $tp->{queue}->enqueue(undef);
+  $tp->join();
+}
+
+
+##==============================================================================
+## Methods: Queue Processing
+
+## undef = PACKAGE::threadMain($tp)
+##   + queue worker sub which wraps init() and free() calls
+sub threadMain {
+  my $tp = shift;
+  $tp->{init}->($tp) if ($tp->{init});
+  do {
+    eval { $tp->process(); };
+    if ($@) {
+      if ($tp->{catch}) {
+	$tp->{catch}->($tp,$@);
+      } else {
+	last;
+      }
+    }
+  } while ($@);
+  $tp->{free}->($tp) if ($tp->{free});
+}
+
+## undef = PACKAGE::process($tp)
+##   + main queue processing loop (can be called e.g. from main thread)
+sub process {
+  my $tp = shift;
+  my ($args);
+  while (defined($args=$tp->{queue}->dequeue)) {
+    $tp->{work}->($tp,@$args) if ($tp->{work});
+  }
+  $tp->{queue}->enqueue(undef); ##-- re-enqueue end-of-queue marker to catch other threads
+}
+
+##==============================================================================
+## Methods: Default Callbacks
+
+## undef = init($tp)
+##  + default thread initializer (no-op)
+sub init { ; }
+
+## undef = work($tp)
+##  + default thread queue-processor (no-op)
+sub work { ; }
+
+## undef = catch($tp)
+##  + die() catcher
+sub catch {
+  my ($tp,$msg) = @_;
+  $tp->vlog($tp->{catchLogLevel},$msg);
+  if ($tp->{catchKillsThread} || $tp->{catchKillsProcess}) {
+    $tp->{free}->($tp) if ($tp->{free});           ##-- run free callback function before exiting
+    exit(1) if ($tp->{catchKillsProcess});         ##-- ... exit() the whole process
+    threads->exit(1) if (threads->can('exit'));    ##-- ... kill the current thread (if we can)
+    exit(1);                                       ##-- ... and otherwise the whole process
+  }
+}
+
+## undef = free($tp)
+##  + default thread cleanup function (no-op)
+sub free { ; }
+
+
+
+1; ##-- be happy
+
+__END__

CAB/Thread/Semaphores.pm
@@ -1,0 +1,161 @@
+## -*- Mode: CPerl -*-
+##
+## File: DTA::CAB::Thread::Semaphores.pm
+## Author: Bryan Jurish <jurish@uni-potsdam.de>
+## Description: generic semaphore pool for DTA::CAB pseudo-locking
+
+package DTA::CAB::Thread::Semaphores;
+use threads;
+use Thread::Semaphore;
+use Carp;
+use strict;
+
+##==============================================================================
+## Globals
+##==============================================================================
+
+our @ISA = qw();
+
+##==============================================================================
+## Constructors etc.
+##==============================================================================
+
+## $ts = CLASS_OR_OBJ->new(%args)
+##  + %$tp, %args:
+##    {
+##     ##-- High-level options
+##     labs => \@labs,          ##-- symbolic semaphore labels
+##     count => $count,         ##-- initial count (default=1)
+##     ##
+##     ##-- Low-level data
+##     sems => \%lab2semaphore, ##-- maps labels to Thread::Semaphore objects (may be undef)
+##    }
+sub new {
+  my $that = shift;
+  my $ts = bless {
+		  labs => [],
+		  count => 1,
+		  sems => {},
+		  @_,
+		 }, ref($that)||$that;
+  return $ts->init();
+}
+
+##==============================================================================
+## Methods: Object Maintainence
+
+## $ts = $ts->init()
+## $ts = $ts->init($count)
+##  + creates a semaphore in $ts->{sems} with count=$count for all un-semaphored labels in $ts->{labs}
+sub init {
+  my ($ts,$count) = @_;
+  $count = $ts->{count} if (!defined($count));
+  foreach (@{$ts->{labs}}) {
+    $ts->{sems}{$_} = Thread::Semaphore->new($count) if (!defined($ts->{sems}{$_}));
+  }
+  return $ts;
+}
+
+## $ts = $ts->reset()
+## $ts = $ts->reset($count)
+##  + deletes and re-creates all semaphores with count=$count
+##  + DANGEROUS
+sub reset {
+  my ($ts,$count) = @_;
+  %{$ts->{sem}} = qw();
+  return $ts->init($count);
+}
+
+## $ts = $ts->clear()
+##  + deletes all labels and associated semaphores
+sub clear {
+  my $ts = shift;
+  @{$ts->{labs}} = qw();
+  %{$ts->{sems}} = qw();
+  return $ts;
+}
+
+## @semaphores = $ts->semaphores()
+##  + list of all semaphores
+sub semaphores {
+  return values %{$_[0]{sems}};
+}
+
+## @threads = $ts->labels()
+##  + list of all semaphore labels
+sub labels {
+  return @{$_[0]{labs}};
+}
+
+## @semaphores = $ts->find(@labels)
+##  + gets semaphores for @labels (no implicit auto-creation)
+sub find {
+  return @{$_[0]{sems}}{@_[1..$#_]};
+}
+
+## @semaphores = $ts->get(@labels)
+##  + gets semaphores for @labels, with implicit auto-creation
+BEGIN {
+  *add = \&get;
+}
+sub get {
+  my $ts = shift;
+  my @sems = qw();
+  my ($sem);
+  foreach (@_) {
+    if (!defined($sem=$ts->{sems}{$_})) {
+      $sem = $ts->{sems}{$_} = Thread::Semaphore->new($ts->{count});
+      push(@{$ts->{labs}}, $_);
+    }
+    push(@sems,$sem);
+  }
+  return @sems;
+}
+
+##==============================================================================
+## Methods: Semaphore Wrappers
+
+## undef = $ts->down($label)
+## undef = $ts->down($label,$count)
+##  + decrement semaphore for $label by $count, e.g. "reserve $count instances of $label resource"
+##  + blocks if semaphore count would drop below zero
+##  + should be followed by $ts->up($label,$count)
+sub down {
+  my ($ts,$label,$count) = @_;
+  $count = $ts->{count} if (!defined($count));
+  my ($sem) = $ts->get($label);
+  $sem->down($count);
+}
+
+## undef = $ts->up($label)
+## undef = $ts->up($label,$count)
+##  + increment semaphore for $label by $count, e.g. "free $count instances of $label resource"
+sub up {
+  my ($ts,$label,$count) = @_;
+  $count = $ts->{count} if (!defined($count));
+  my ($sem) = $ts->get($label);
+  $sem->up($count);
+}
+
+## $rc = $ts->downup($label,\&sub)
+## $rc = $ts->downup($label,\&sub,$count)
+##  + like:
+##    {
+##     $ts->down($label,$count);
+##     sub();
+##     $ts->up($label,$count);
+##    }
+##  + but wrapped in an eval BLOCK to ensure that up() gets called
+sub downup {
+  my ($ts,$label,$sub,$count) = @_;
+  $ts->down($label,$count);
+  my ($rc);
+  eval { $rc = $sub->(); };
+  $ts->up($label,$count);
+  die($@) if ($@);
+  return $rc;
+}
+
+1; ##-- be happy
+
+__END__

commit 977a0b9b520a8ae536903bfe74c9cee54ddb4bb7

+ fixed PatternLayoutl typo in Logger.pm (introduced in r5410)
git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@13717 4a515de8-beb3-11dc-84d6-2b654028afac

CAB/Logger.pm
@@ -155,7 +155,7 @@ log4perl.appender.AppFile = Log::Log4perl::Appender::File
 log4perl.appender.AppFile.filename = $opts{file}
 log4perl.appender.AppFile.mode = append
 log4perl.appender.AppFile.utf8 = 1
-log4perl.appender.AppFile.layout = Log::Log4perl::Layout::PatternLayoutl
+log4perl.appender.AppFile.layout = Log::Log4perl::Layout::PatternLayout
 log4perl.appender.AppFile.layout.ConversionPattern = %d{yyyy-MM-dd HH:mm:ss} [%P] (%p) %c: %m%n
 ";
   }

CAB/Version.pm
@@ -1,5 +1,5 @@
 ##-- automatically generated file: DO NOT EDIT!
 package DTA::CAB::Version;
 $DTA::CAB::VERSION = '1.66';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 13441';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 13697M';
 1;