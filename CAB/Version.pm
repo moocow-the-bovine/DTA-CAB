89bfc1c96d11c118cf2dc45ece2eacc608b9734b
Bryan Jurish <jurish@bbaw.de>
2016-06-16 10:09:18 +0000

    + removed debug code from dta-cab-analyze.perl
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@16221 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -5,5 +5,5 @@
 ##
 package DTA::CAB::Version;
 $DTA::CAB::VERSION = '1.78';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 16219';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 16220M';
 1;
