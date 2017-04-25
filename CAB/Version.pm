7fdf1a12b3e383e2bc7df43925ea729fcbffb5f9
Bryan Jurish <jurish@bbaw.de>
2017-04-25 10:34:48 +0000

    + webservicehowto url tweaks (bbaw epub server URLs moved)
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@26251 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -5,5 +5,5 @@
 ##
 package DTA::CAB::Version;
 $DTA::CAB::VERSION = '1.83';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 26158M';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 26231:26232M';
 1;
