a365db8805937a42ddff2d6926865b36c03e4408
Bryan Jurish <jurish@bbaw.de>
2017-05-02 07:57:53 +0000

    + v1.86: updates for dta-cab-version.perl
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@26360 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -5,5 +5,5 @@
 ##
 package DTA::CAB::Version;
 $DTA::CAB::VERSION = '1.85';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 26347';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 26355';
 1;
