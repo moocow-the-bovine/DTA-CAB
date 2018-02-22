1d2b00c5e506ec97f92eb906e88f1544be64622c
Bryan Jurish <jurish@bbaw.de>
2018-02-22 09:54:47 +0000

    + clean Version.pm
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@27798 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -5,5 +5,5 @@
 ##
 package DTA::CAB::Version;
 $DTA::CAB::VERSION = '1.99';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 27793';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 27797';
 1;
