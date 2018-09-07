fd55e335e54954b20a5347f191347aa14dfef01d
Bryan Jurish <jurish@bbaw.de>
2018-09-07 10:35:39 +0000

    + updated 'fliegen' example in web-howto
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@28574 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -5,5 +5,5 @@
 ##
 package DTA::CAB::Version;
 $DTA::CAB::VERSION = '1.103';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 28451';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 28452';
 1;
