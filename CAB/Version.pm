c2787a319dc3915cd1fe6aa1262308b3dff77a93
Bryan Jurish <jurish@bbaw.de>
2019-02-11 13:58:27 +0000

    + v1.106  - SVNVERSION tweaks (revision only, no root URL)  - nofollow.rpl: File::Find hacks for ExtUtils::Manifest
    git-svn-id: file:///home/svn/dev/DTA-CAB/trunk@29503 4a515de8-beb3-11dc-84d6-2b654028afac

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -5,5 +5,5 @@
 ##
 package DTA::CAB::Version;
 $DTA::CAB::VERSION = '1.105';
-$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 29318';
+$DTA::CAB::SVNVERSION = 'svn+ssh://odo.dwds.de/home/svn/dev/DTA-CAB/trunk @ 29497:29502M';
 1;
