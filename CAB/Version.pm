dc0aa4201e5c32de93270fd4b53a883a6234fbee
Bryan Jurish <jurish@bbaw.de>
2021-07-20 13:33:01 +0200

    + added .gitignore (from old svn:ignore)

--- CAB/Version.pm
+++ CAB/Version.pm
@@ -5,5 +5,5 @@
 ##
 package DTA::CAB::Version;
 $DTA::CAB::VERSION = '1.115';
-$DTA::CAB::SVNVERSION = '34218';
+$DTA::CAB::SVNVERSION = 'git@github.com:moocow-the-bovine/DTA-CAB.git#a07f8210f1554545406d78bda50c1c75fc44e506';
 1;
